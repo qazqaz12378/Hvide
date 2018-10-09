86a150815e382ef8a0135d216f67deb5f170fdc0
hyc <290513701@qq.com>
2018-10-09 22:54:03 +0800

    添加横竖屏自动切换功能

--- app/src/main/java/com/example/hyc/ffmpegtest/MainActivity.java
+++ app/src/main/java/com/example/hyc/ffmpegtest/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.hyc.ffmpegtest;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private DNPlayer dnPlayer;
-
+    private  SurfaceView surfaceView;
+    private int height = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+        surfaceView = findViewById(R.id.surfaceView);
+        ViewGroup.LayoutParams m = surfaceView.getLayoutParams();
+        final float scale = this.getResources().getDisplayMetrics().density;
+        height = (int)(200 * scale + 0.5f);
         dnPlayer = new DNPlayer();
         dnPlayer.setSurfaceView(surfaceView);
         dnPlayer.setDataSource("/sdcard/dong.mp4");
@@ -35,5 +42,27 @@ public class MainActivity extends AppCompatActivity {
     }
     public void start(View view){
         dnPlayer.prepare();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            //横屏
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager
+                    .LayoutParams.FLAG_FULLSCREEN);
+            ViewGroup.LayoutParams m = surfaceView.getLayoutParams();
+            m.width = m.MATCH_PARENT;
+            m.height = m.MATCH_PARENT;
+            surfaceView.setLayoutParams(m);
+        } else {
+            //竖屏
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            ViewGroup.LayoutParams m = surfaceView.getLayoutParams();
+            m.width = m.MATCH_PARENT;
+            m.height = height;
+            surfaceView.setLayoutParams(m);
+        }
+
     }
 }
